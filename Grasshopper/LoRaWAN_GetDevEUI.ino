abe207e8da4bd5e0daa49dfa3702397d6f118b24
Kris Winer <kriswiner@users.noreply.github.com>
2018-02-05 12:47:42 -0800

    Update LoRaWAN_GetDevEUI.ino

--- Grasshopper/LoRaWAN_GetDevEUI.ino
+++ Grasshopper/LoRaWAN_GetDevEUI.ino
@@ -8,11 +8,11 @@
 char buffer[32];
 
 void setup( void ) {
-  Serial1.begin(9600);
+  Serial.begin(9600);
   LoRaWAN.begin(EU868);
-  Serial1.println("devEUI request");
+  Serial.println("devEUI request");
   LoRaWAN.getDevEui(buffer,18);
-  Serial1.println(buffer);
+  Serial.println(buffer);
 }
 
 void loop( void ) {
