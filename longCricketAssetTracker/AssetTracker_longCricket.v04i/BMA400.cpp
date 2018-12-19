ae6af734d383ba8c4c41a62cfb5e6a59d531f9fe
Kris Winer <kriswiner@users.noreply.github.com>
2018-12-19 14:32:26 -0800

    Add files via upload

---
+++ longCricketAssetTracker/AssetTracker_longCricket.v04i/BMA400.cpp
@@ -0,0 +1,284 @@
+/* 10/16/2018 Copyright Tlera Corporation
+ *  
+ *  Created by Kris Winer
+ *  
+ *  The BMA400 is an inexpensive (~$2), three-axis, medium-resolution (12-bit), ultra-low power (800 nA low power mode) accelerometer 
+ *  in a tiny 2 mm x 2 mm LGA12 package with 1024-byte FIFO, 
+ *  two multifunction interrupts and widely configurable sample rate (15 - 800 Hz), full range (2 - 16 g), low power modes, 
+ *  and interrupt detection behaviors. This accelerometer is nice choice for motion-based wake/sleep,
+ *  tap detection, step counting, and simple orientation estimation.
+ *  
+ *  Library may be used freely and without limit with attribution.
+ *  
+ */
+#include "BMA400.h"
+
+BMA400::BMA400(uint8_t intPin1, uint8_t intPin2)
+{
+  _intPin1 = intPin1;
+  _intPin2 = intPin2;   
+}
+
+
+uint8_t BMA400::getChipID()
+{
+  uint8_t c = readByte(BMA400_ADDRESS, BMA400_CHIPID);
+  return c;
+}
+
+uint8_t BMA400::getStatus()
+{
+  uint8_t c = readByte(BMA400_ADDRESS, BMA400_INT_STAT_0);
+  return c;
+}
+
+float  BMA400::getAres(uint8_t Ascale) {
+  switch (Ascale)
+  {
+   // Possible accelerometer scales (and their register bit settings) are:
+   // 2 Gs , 4 Gs , 8 Gs , and 16 Gs . 
+    case AFS_2G:
+          _aRes = 2.0f/2048.0f;// per data sheet
+          return _aRes;
+          break;
+    case AFS_4G:
+          _aRes = 4.0f/2048.0f;
+          return _aRes;
+          break;
+    case AFS_8G:
+          _aRes = 8.0f/2048.0f;
+          return _aRes;
+          break;
+    case AFS_16G:
+          _aRes = 16.0f/2048.0f;
+          return _aRes;
+          break;
+  }
+}
+
+void BMA400::initBMA400(uint8_t Ascale, uint8_t SR, uint8_t power_Mode, uint8_t OSR, uint8_t acc_filter)
+{
+   /* Normal mode configuration */
+   // set bandwidth to 0.2x sample rate (bit 7), OSR in low-power mode, power mode
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG0, 0x80 | OSR << 5 | power_Mode);
+   delay(2); // wait 1.5 ms
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG1, Ascale << 6 | OSR << 4 | SR); // set full-scale range, oversampling and sample rate
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG2, acc_filter << 2);             // set accel filter
+   
+   /* Low power mode configuration */
+// writeByte(BMA400_ADDRESS,BMA400_AUTOLOWPOW_0, 0xFA);             // set auto low power time out to 10 second
+// writeByte(BMA400_ADDRESS,BMA400_AUTOLOWPOW_1, 0x04);             // switch to low power after time out (max 10.2 sec)
+   writeByte(BMA400_ADDRESS,BMA400_AUTOLOWPOW_1, 0x02);             // switch to low power when GEN1 interrupt triggered
+
+   writeByte(BMA400_ADDRESS,BMA400_AUTOWAKEUP_1, 0x02);             // enable interrupt for auto wake up
+   // enable interrupt on all three axes (bits 5 - 7)for auto wake up
+   // use 4 samples (n + 1) for wakeup condition (bits 4 - 2), 4 samples = 160 ms at 25 Hz
+   // reference updated whenever going into low power mode (bits 1, 0)
+   writeByte(BMA400_ADDRESS,BMA400_WKUP_INT_CONFIG0, 0x07 << 5 | 0x03 << 2 | 0x01); 
+   writeByte(BMA400_ADDRESS,BMA400_WKUP_INT_CONFIG1, 0x08); //  50 mg threshold for wake on any axis, n x 6.25 mgs at 2 g FS
+ 
+   /* Interrupt configuration */
+   // Map interrupts and set interrupt behavior
+   writeByte(BMA400_ADDRESS,BMA400_INT1_MAP, 0x04);              // map WKUP interrupt to INT1 (bit 0) 
+   writeByte(BMA400_ADDRESS,BMA400_INT2_MAP, 0x01);              // map GEN1 interrupt to INT2 (bit 2)
+   writeByte(BMA400_ADDRESS,BMA400_INT12_IO_CTRL, 0x20 | 0x02);  // set both interrupts push-pull, active HIGH 
+
+   // Config GEN1 on INT2
+   writeByte(BMA400_ADDRESS,BMA400_GEN1INT_CONFIG0, 0xFA);       // all axes, fixed 100 Hz, reference every time, hysteresis 48 mg
+   writeByte(BMA400_ADDRESS,BMA400_GEN1INT_CONFIG1, 0x01);       // inactive motion interrupt configuration
+   writeByte(BMA400_ADDRESS,BMA400_GEN1INT_CONFIG2, 0x03);       // set threshold 24 mg (8 mg per count)
+   writeByte(BMA400_ADDRESS,BMA400_GEN1INT_CONFIG31,0x40);       // measured over 64 data samples
+
+  /* Enable interrupts */
+   writeByte(BMA400_ADDRESS,BMA400_INT_CONFIG0,  0x04);          // enable GEN1 interrupt (bit 2) 
+}
+
+
+void BMA400::activateNoMotionInterrupt()
+{
+  writeByte(BMA400_ADDRESS,BMA400_INT_CONFIG0,  0x04);           // enable GEN1 (no_Motion) interrupt  
+}
+
+void BMA400::deactivateNoMotionInterrupt()
+{
+  writeByte(BMA400_ADDRESS,BMA400_INT_CONFIG0,  0x00);           // disable GEN1 (no_Motion) interrupt  
+}
+
+
+void BMA400::CompensationBMA400(uint8_t Ascale, uint8_t SR, uint8_t power_Mode, uint8_t OSR, uint8_t acc_filter, float * offset)
+{
+     Serial.println("hold flat and motionless for bias calibration");
+     delay(5000);
+     
+     writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG0, 0x80 | OSR << 5 | power_Mode);// set bandwidth to 0.2x sample rate, OSR in low-power mode, power mode
+     delay(2);
+     writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG1, Ascale << 6 | OSR << 4 | SR); // set full-scale range, oversampling and sample rate
+     writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG2, acc_filter << 2);             // set accel filter
+
+     int16_t temp[3] = {0, 0, 0};
+     int32_t sum[3] = {0, 0, 0};
+
+     for(uint8_t ii = 0; ii < 128; ii++)
+     {
+       readBMA400AccelData(temp);
+       sum[0] += temp[0];
+       sum[1] += temp[1];
+       sum[2] += temp[2];
+       delay(100);
+     }
+     
+     offset[0] = float(sum[0])/128.0f;
+     offset[1] = float(sum[1])/128.0f;
+     offset[2] = float(sum[2])/128.0f;
+     offset[0] *= _aRes;
+     offset[1] *= _aRes;
+     offset[2] *= _aRes;
+     if(offset[2] > +0.5f) offset[2] = offset[2] - 1.0f;
+     if(offset[2] < -0.5f) offset[2] = offset[2] + 1.0f;
+
+     Serial.print("x-axis offset = "); Serial.print(offset[0]*1000.0f, 1); Serial.println(" mg");
+     Serial.print("y-axis offset = "); Serial.print(offset[1]*1000.0f, 1); Serial.println(" mg");
+     Serial.print("z-axis offset = "); Serial.print(offset[2]*1000.0f, 1); Serial.println(" mg");
+     /* end of accel calibration */
+     }
+
+
+   void BMA400::resetBMA400()
+   {
+    writeByte(BMA400_ADDRESS,BMA400_CMD,0xB6); // software reset the BMA400
+   }
+
+
+   void BMA400::selfTestBMA400()
+   {
+   int16_t temp[3]={0, 0, 0};
+   int16_t posX, posY, posZ, negX, negY, negZ;
+   
+   // initialize sensor for self test
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG0, 0x80 | osr3 << 5 | normal_Mode);// set bandwidth to 0.2x sample rate, OSR in low-power mode, power mode
+   delay(2);
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG1, AFS_4G << 6 | osr3 << 4 | SR_100Hz); // set full-scale range to +/- 4 g
+   writeByte(BMA400_ADDRESS,BMA400_ACC_CONFIG2, acc_filt1 << 2);             // set accel filter
+   float STres = 4000.0f/2048.0f; // mg/LSB for 4 g full scale
+   delay(100);
+   
+   // positive axes test
+   writeByte(BMA400_ADDRESS,BMA400_SELF_TEST, 0x00 | 0x07); // positive axes
+   delay(100);
+   readBMA400AccelData(temp);
+   posX = temp[0];
+   posY = temp[1];
+   posZ = temp[2];
+
+   // negative axes test
+   writeByte(BMA400_ADDRESS,BMA400_SELF_TEST, 0x08 | 0x07); // negative axes
+   delay(100);
+   readBMA400AccelData(temp);
+   negX = temp[0];
+   negY = temp[1];
+   negZ = temp[2];
+
+   Serial.print("x-axis self test = "); Serial.print((float)(posX - negX)*STres, 1); Serial.println("mg, should be > 2000 mg");
+   Serial.print("y-axis self test = "); Serial.print((float)(posY - negY)*STres, 1); Serial.println("mg, should be > 1800 mg");
+   Serial.print("z-axis self test = "); Serial.print((float)(posZ - negZ)*STres, 1); Serial.println("mg, should be > 800 mg");
+
+   writeByte(BMA400_ADDRESS,BMA400_SELF_TEST, 0x00); // disable self test
+/* end of self test*/
+}
+
+
+void BMA400::readBMA400AccelData(int16_t * destination)
+{
+  uint8_t rawData[6];  // x/y/z accel register data stored here
+  readBytes(BMA400_ADDRESS, BMA400_ACCD_X_LSB, 6, &rawData[0]);  // Read the 6 raw data registers into data array
+  destination[0] = ( (rawData[1] & 0x0F) << 8) | rawData[0];     // Turn the MSB and LSB into a signed 12-bit value
+  destination[1] = ( (rawData[3] & 0x0F) << 8) | rawData[2];  
+  destination[2] = ( (rawData[5] & 0x0F) << 8) | rawData[4]; 
+  if(destination[0] > 2047) destination[0] += -4096;
+  if(destination[1] > 2047) destination[1] += -4096;
+  if(destination[2] > 2047) destination[2] += -4096;
+}
+
+
+int16_t BMA400::readBMA400TempData()
+{
+  uint8_t temp = readByte(BMA400_ADDRESS, BMA400_TEMP_DATA);    // Read the raw data register  
+  int16_t tmp = (int16_t) ( ((int16_t)temp << 8) | 0x00) >> 8;  // Turn into signed 8-bit temperature value
+  return tmp;
+  }
+
+
+// I2C scan function
+void BMA400::I2Cscan()
+{
+// scan for i2c devices
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmission to see if
+    // a device did acknowledge to the address.
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+    
+}
+
+// I2C read/write functions for the BMA400 sensor
+
+  void BMA400::writeByte(uint8_t address, uint8_t subAddress, uint8_t data)
+{
+  Wire.beginTransmission(address);  // Initialize the Tx buffer
+  Wire.write(subAddress);           // Put slave register address in Tx buffer
+  Wire.write(data);                 // Put data in Tx buffer
+  Wire.endTransmission();           // Send the Tx buffer
+}
+
+  uint8_t BMA400::readByte(uint8_t address, uint8_t subAddress)
+{
+  uint8_t data = 0;                        // `data` will store the register data   
+  Wire.beginTransmission(address);         // Initialize the Tx buffer
+  Wire.write(subAddress);                  // Put slave register address in Tx buffer
+  Wire.endTransmission(false);             // Send the Tx buffer, but send a restart to keep connection alive
+  Wire.requestFrom(address, 1);            // Read one byte from slave register address  
+  data = Wire.read();                      // Fill Rx buffer with result
+  return data;                             // Return data read from slave register
+}
+
+  void BMA400::readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest)
+{  
+  Wire.beginTransmission(address);   // Initialize the Tx buffer
+  Wire.write(subAddress);            // Put slave register address in Tx buffer
+  Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+  uint8_t i = 0;
+  Wire.requestFrom(address, count);  // Read bytes from slave register address 
+  while (Wire.available()) {
+        dest[i++] = Wire.read(); }   // Put read results in the Rx buffer
+}
---
+++ longCricketAssetTracker/AssetTracker_longCricket.v04i/BMA400.h
@@ -0,0 +1,175 @@
+/* 10/16/2018 Copyright Tlera Corporation
+ *  
+ *  Created by Kris Winer
+ *  
+ *  The BMA400 is an inexpensive (~$2), three-axis, medium-resolution (12-bit), ultra-low power (800 nA low power mode) accelerometer 
+ *  in a tiny 2 mm x 2 mm LGA12 package with 1024-byte FIFO, 
+ *  two multifunction interrupts and widely configurable sample rate (15 - 800 Hz), full range (2 - 16 g), low power modes, 
+ *  and interrupt detection behaviors. This accelerometer is nice choice for motion-based wake/sleep,
+ *  tap detection, step counting, and simple orientation estimation.
+ *  
+ *  Library may be used freely and without limit with attribution.
+ *  
+ */
+ 
+#ifndef BMA400_h
+#define BMA400_h
+
+#include "Arduino.h"
+#include <Wire.h>
+
+/* Register Map BMA400
+// https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BMA400-DS000-02.pdf
+*/
+#define BMA400_CHIPID          0x00
+
+#define BMA400_ERR_REG         0x02
+#define BMA400_STATUS          0x03
+#define BMA400_ACCD_X_LSB      0x04
+#define BMA400_ACCD_X_MSB      0x05
+#define BMA400_ACCD_Y_LSB      0x06
+#define BMA400_ACCD_Y_MSB      0x07
+#define BMA400_ACCD_Z_LSB      0x08
+#define BMA400_ACCD_Z_MSB      0x09
+#define BMA400_SENSOR_TIME_0   0x0A
+#define BMA400_SENSOR_TIME_1   0x0B
+#define BMA400_SENSOR_TIME_2   0x0C
+#define BMA400_EVENT           0x0D
+#define BMA400_INT_STAT_0      0x0E
+#define BMA400_INT_STAT_1      0x0F
+#define BMA400_INT_STAT_2      0x10
+#define BMA400_TEMP_DATA       0x11
+#define BMA400_FIFO_LENGTH0    0x12
+#define BMA400_FIFO_LENGTH1    0x13
+#define BMA400_FIFO_DATA       0x14
+#define BMA400_STEP_CNT_0      0x15
+#define BMA400_STEP_CNT_1      0x16
+#define BMA400_STEP_CNT_2      0x17
+#define BMA400_STEP_STAT       0x18
+#define BMA400_ACC_CONFIG0     0x19
+#define BMA400_ACC_CONFIG1     0x1A
+#define BMA400_ACC_CONFIG2     0x1B
+
+#define BMA400_INT_CONFIG0     0x1F
+#define BMA400_INT_CONFIG1     0x20
+#define BMA400_INT1_MAP        0x21
+#define BMA400_INT2_MAP        0x22
+#define BMA400_INT12_MAP       0x23
+#define BMA400_INT12_IO_CTRL   0x24
+
+#define BMA400_FIFO_CONFIG0    0x26
+#define BMA400_FIFO_CONFIG1    0x27
+#define BMA400_FIFO_CONFIG2    0x28
+#define BMA400_FIFO_PWR_CONFIG 0x29
+#define BMA400_AUTOLOWPOW_0    0x2A
+#define BMA400_AUTOLOWPOW_1    0x2B
+#define BMA400_AUTOWAKEUP_0    0x2C
+#define BMA400_AUTOWAKEUP_1    0x2D
+
+#define BMA400_WKUP_INT_CONFIG0 0x2F
+#define BMA400_WKUP_INT_CONFIG1 0x30
+#define BMA400_WKUP_INT_CONFIG2 0x31
+#define BMA400_WKUP_INT_CONFIG3 0x32
+#define BMA400_WKUP_INT_CONFIG4 0x33
+
+#define BMA400_ORIENTCH_CONFIG0 0x35
+#define BMA400_ORIENTCH_CONFIG1 0x36
+#define BMA400_ORIENTCH_CONFIG2 0x37
+#define BMA400_ORIENTCH_CONFIG3 0x38
+#define BMA400_ORIENTCH_CONFIG4 0x39
+#define BMA400_ORIENTCH_CONFIG5 0x3A
+#define BMA400_ORIENTCH_CONFIG6 0x3B
+#define BMA400_ORIENTCH_CONFIG7 0x3C
+#define BMA400_ORIENTCH_CONFIG8 0x3D
+#define BMA400_ORIENTCH_CONFIG9 0x3E
+
+#define BMA400_GEN1INT_CONFIG0  0x3F
+#define BMA400_GEN1INT_CONFIG1  0x40
+#define BMA400_GEN1INT_CONFIG2  0x41
+#define BMA400_GEN1INT_CONFIG3  0x42
+#define BMA400_GEN1INT_CONFIG31 0x43
+#define BMA400_GEN1INT_CONFIG4  0x44
+#define BMA400_GEN1INT_CONFIG5  0x45
+#define BMA400_GEN1INT_CONFIG6  0x46
+#define BMA400_GEN1INT_CONFIG7  0x47
+#define BMA400_GEN1INT_CONFIG8  0x48
+#define BMA400_GEN1INT_CONFIG9  0x49
+#define BMA400_GEN2INT_CONFIG0  0x4A
+#define BMA400_GEN2INT_CONFIG1  0x4B
+#define BMA400_GEN2INT_CONFIG2  0x4C
+#define BMA400_GEN2INT_CONFIG3  0x4D
+#define BMA400_GEN2INT_CONFIG31 0x4E
+#define BMA400_GEN2INT_CONFIG4  0x4F
+#define BMA400_GEN2INT_CONFIG5  0x50
+#define BMA400_GEN2INT_CONFIG6  0x51
+#define BMA400_GEN2INT_CONFIG7  0x52
+#define BMA400_GEN2INT_CONFIG8  0x53
+#define BMA400_GEN2INT_CONFIG9  0x54
+#define BMA400_ACTCH_CONFIG0    0x55
+#define BMA400_ACTCH_CONFIG1    0x56
+#define BMA400_TAP_CONFIG       0x57
+#define BMA400_TAP_CONFIG1      0x58
+
+#define BMA400_IF_CONF          0x7C
+#define BMA400_SELF_TEST        0x7D
+#define BMA400_CMD              0x7E
+
+
+#define BMA400_ADDRESS  0x14  // if ADO is 0 (default)
+
+
+#define AFS_2G           0x00
+#define AFS_4G           0x01
+#define AFS_8G           0x02
+#define AFS_16G          0x03
+
+#define SR_12_5Hz        0x05  // 12.5 Hz sample rate, etc
+#define SR_25Hz          0x06
+#define SR_50Hz          0x07
+#define SR_100Hz         0x08
+#define SR_200Hz         0x09  // 200 Hz sample rate
+#define SR_400Hz         0x0A
+#define SR_800Hz         0x0B  // 800 Hz sample rate  
+
+#define sleep_Mode       0x00  // define power modes
+#define lowpower_Mode    0x01
+#define normal_Mode      0x02
+#define sleep_Mode       0x03
+
+// define oversampling
+#define osr0             0x00  // lowest power, lowest oversampling, lowest accuracy
+#define osr1             0x01
+#define osr2             0x02
+#define osr3             0x03 // highest oversampling, highest power, highest accuracy
+
+#define acc_filt1        0x00 // variable ODR filter
+#define acc_filt2        0x01 // fixed 100 Hz ODR filter
+#define acc_filt_lp      0x02 // fixed 100 Hz ODR filter, 1 Hz BW
+
+
+class BMA400
+{
+  public: 
+  BMA400(uint8_t intPin1, uint8_t intPin2);
+  float getAres(uint8_t Ascale);
+  uint8_t getChipID();
+  void initBMA400(uint8_t Ascale, uint8_t SR, uint8_t power_Mode, uint8_t OSR, uint8_t acc_filter);
+  void CompensationBMA400(uint8_t Ascale, uint8_t SR, uint8_t power_Mode, uint8_t OSR, uint8_t acc_filter, float * offset);
+  void resetBMA400();
+  void selfTestBMA400();
+  void readBMA400AccelData(int16_t * destination);
+  int16_t readBMA400TempData();
+  void activateNoMotionInterrupt();
+  void deactivateNoMotionInterrupt();
+  uint8_t getStatus();
+  void I2Cscan();
+  void writeByte(uint8_t address, uint8_t subAddress, uint8_t data);
+  uint8_t readByte(uint8_t address, uint8_t subAddress);
+  void readBytes(uint8_t address, uint8_t subAddress, uint8_t count, uint8_t * dest);
+  private:
+  uint8_t _intPin1;
+  uint8_t _intPin2;
+  float _aRes;
+};
+
+#endif
