52d39ad6e91b3f510ba3257690f43f4103ae9f6e
Kris Winer <kriswiner@users.noreply.github.com>
2019-06-22 18:34:32 -0700

    Add files via upload

---
+++ Grasshopper/I2CslaveGrasshopper/Grasshopper_Wire_Slave.ino
@@ -0,0 +1,111 @@
+/* Wire Slave example (see Wire_Master.ino for the master)
+ *    
+ * The myReceiveCallback tracking the received data sets
+ * tx_index into tx_data[], if only one byte had been
+ * transferred. The myRequestCallback puts 32 bytes from
+ * tx_data[] starting at tx_index into the transmit buffer.
+ * Finally the myTransmitCallback adjusts tx_index with
+ * the number of transferred bytes.
+ *
+ * The code roughly simluates a slave device with a FIFO.
+ * Sunce the myRequestCallback cannot know how many bytes
+ * need to be send, it fills up the buffer to the max. 
+ * Only at the myTransmitCallback the number  of bytes
+ * transmitted is known.
+ *
+ *    
+ * This example code is in the public domain.
+ */
+
+#include "Wire.h"
+#include <STM32L0.h>
+#include "TimerMillis.h"
+
+TimerMillis SensorTimer;
+
+int tx_index = 0;
+bool newData = true;
+uint8_t intPin =  10, myLed = 13;
+
+// MCU VDDA and temperature definitions
+float VDDA, STM32L0Temp;
+
+//uint8_t tx_data[] = "The quick brown fox jumps over the lazy dog\r\n";
+uint8_t tx_data[4];
+
+void myReceiveCallback(int count)
+{
+    if (count == 1)
+    {
+        tx_index = Wire.read();
+        
+        while (tx_index >= sizeof(tx_data))
+        {
+            tx_index -= sizeof(tx_data);
+        }
+    }
+}
+
+void myRequestCallback(void)
+{
+    for (int i = 0, n = tx_index; i < BUFFER_LENGTH; i++)
+    {
+        Wire.write(tx_data[n]);
+        
+        n++;
+        
+        if (n >= sizeof(tx_data)) { n = 0; }
+    }
+}
+
+void myTransmitCallback(int count)
+{
+    tx_index += count;
+
+    while (tx_index >= sizeof(tx_data))
+    {
+        tx_index -= sizeof(tx_data);
+    }
+}
+
+void mySensorCallback()
+{
+  newData = true; 
+  STM32L0.wakeup();
+}
+
+void setup()
+{
+    pinMode(myLed, OUTPUT);
+    digitalWrite(myLed, LOW); // start with led off since active HIGH
+    pinMode(intPin, OUTPUT);
+  
+    Wire.begin(0x7c);
+
+    Wire.onReceive(myReceiveCallback);
+    Wire.onRequest(myRequestCallback);
+    Wire.onTransmit(myTransmitCallback);
+
+    SensorTimer.start(mySensorCallback, 1000, 1000);      //  1 second period, delayed 1 seconds
+}
+
+void loop()
+{
+  if(newData) 
+  {
+  newData = false;
+  
+  VDDA = STM32L0.getVDDA();
+  STM32L0Temp = STM32L0.getTemperature();
+  
+  tx_data[0] = (((uint16_t) (100.0f * VDDA)) & 0xFF00) >> 8;
+  tx_data[1] = (((uint16_t) (100.0f * VDDA)) & 0x00FF);
+  tx_data[2] = (((uint16_t) (100.0f * STM32L0Temp)) & 0xFF00) >> 8;
+  tx_data[3] = (((uint16_t) (100.0f * STM32L0Temp)) & 0x00FF);
+
+  digitalWrite(intPin, HIGH); delay(1); digitalWrite(intPin,LOW);
+  digitalWrite(myLed, HIGH);  delay(1); digitalWrite(myLed,LOW);
+  }
+
+  STM32L0.stop();
+}
---
+++ Grasshopper/I2CslaveGrasshopper/Master_I2C_Dragonfly.ino
@@ -0,0 +1,115 @@
+#include <Wire.h>
+
+uint8_t intPin = 9;
+
+uint8_t rx_data[4] = {0, 0, 0, 0};
+bool newData = false;
+float VDDA = 0.0f, Temp = 0.0f;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(4000);
+
+  pinMode(intPin, INPUT);
+ 
+  Wire.begin(TWI_PINS_20_21); // set master mode 
+  Wire.setClock(400000);      // I2C frequency at 400 kHz 
+  delay(1000);
+  
+  I2Cscan();
+
+  Serial.println("I2C scan done");
+
+  attachInterrupt(intPin, myInterruptHandler, RISING);
+
+}
+
+
+void loop() {
+  
+  if(newData)
+  {
+  newData = false;
+
+  readBytes(0x7C, 4, rx_data);
+  for (uint8_t i = 0; i < 4; i++)
+  {
+    Serial.print("0x"); Serial.println(rx_data[i], HEX);
+  }
+  Serial.println(" ");
+  
+  VDDA = (float) ( (uint16_t)rx_data[0] << 8 | (uint16_t)rx_data[1] );
+  VDDA /= 100.0f;
+  Temp = (float) ( (uint16_t)rx_data[2] << 8 | (uint16_t)rx_data[3]);
+  Temp /= 100.0f;
+  Serial.print(" VDDA = "); Serial.print(VDDA, 2); Serial.println(" V");
+  Serial.print(" Temp = "); Serial.print(Temp, 2); Serial.println(" C");
+  Serial.println(" ");
+  }
+
+}
+
+// Useful function
+
+void myInterruptHandler()
+{
+newData = true;
+}
+
+// I2C scan function
+void I2Cscan()
+{
+// scan for i2c devices
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmission to see if
+    // a device did acknowledge to the address.
+//    Wire.beginTransmission(address);
+//    error = Wire.endTransmission();
+      error = Wire.transfer(address, NULL, 0, NULL, 0);
+      
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+    
+}
+
+  void readBytes(uint8_t address, uint8_t count, uint8_t * dest)
+{  
+  Wire.beginTransmission(address);   // Initialize the Tx buffer
+//  Wire.write(subAddress);            // Put slave register address in Tx buffer
+  Wire.endTransmission(false);       // Send the Tx buffer, but send a restart to keep connection alive
+  uint8_t i = 0;
+  Wire.requestFrom(address, (size_t) count);  // Read bytes from slave register address 
+  while (Wire.available()) 
+        {
+        dest[i++] = Wire.read(); 
+        }         // Put read results in the Rx buffer
+}
